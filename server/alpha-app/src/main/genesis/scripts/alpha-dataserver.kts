3c84e591f8befb48aa86c4fbf6553876a99e4456
Guilherme Guerra Martins <guilherme.guerra@genesis.global>
2024-04-16 13:42:44 +0000

    add basic configurations to training seed

--- server/alpha-app/src/main/genesis/scripts/alpha-dataserver.kts
+++ server/alpha-app/src/main/genesis/scripts/alpha-dataserver.kts
@@ -16,11 +16,6 @@ package scripts
 
 dataServer {
     query("ALL_TRADES", TRADE_VIEW) {
-        permissioning {
-            auth(mapName = "ENTITY_VISIBILITY") {
-                TRADE_VIEW.COUNTERPARTY_ID
-            }
-        }
     }
     query("ALL_PRICES", TRADE){
         fields{
